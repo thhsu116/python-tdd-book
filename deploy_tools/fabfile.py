5966cfccb77e948f3b149196f7e174433e190a01
thhsu <thhsu@qti.qualcomm.com>
2018-10-07 22:33:37 -0700

    add fabric and deployment script

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,48 @@
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+REPO_URL = 'https://github.com/thhsu116/python-tdd-book.git'
+
+def deploy():
+    if 'staging' in env.host.lower():
+        site_folder = f'/home/{env.user}/sites/python-tdd-staging'
+    else:
+        site_folder = f'/home/{env.user}/sites/python-tdd'
+    #site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mk dir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtulaenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+        
+def _get_latest_source():
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {REPO_URL} . ')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
+    
+def _update_virtualenv():
+    if not exists('virtualenv/bin/pip'):
+        run(f'python3.6 -m venv virtualenv')
+    run('./virtualenv/bin/pip install -r requirements.txt')
+    
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        
+def _update_static_files():
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+    
+def _update_database():
+    run('./virtualenv/bin/python manage.py migrate --noinput')
